0dc87de19c5a493cedab3728d6f6c16f24487c6e
1449902124@qq.com <1449902124@qq.com>
2019-01-24 10:23:04 +0800

    每日一题

--- 2019-1-23.go
+++ 2019-1-23.go
@@ -37,10 +37,10 @@ import "fmt"
 
 //Definition for singly-linked list.
 
-type ListNode struct {
-Val int
-Next *ListNode
-}
+//type ListNode struct {
+//Val int
+//Next *ListNode
+//}
 
 
 func hasCycle(head *ListNode) bool {
